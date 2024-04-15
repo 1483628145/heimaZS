a063b152f53442495cd3f42e4ff353801dd06a79
lxb <1483628145@qq.com>
2024-04-15 10:51:52 +0800

    init

---
+++ src/api/user.js
@@ -0,0 +1,13 @@
+import request from '@/utils/request'
+
+/**
+ * 登录接口
+ *
+ * **/
+export function login(data) {
+  return request({
+    url: '/park/login',
+    method: 'POST',
+    data
+  })
+}
---
+++ src/constants/KEY.js
@@ -0,0 +1,2 @@
+// 放置公共产量
+export const TOKEN_KEY = 'token_key'
